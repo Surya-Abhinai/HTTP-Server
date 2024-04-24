cccc4968db57b70f4dc7cda30e16b5ea3fc289be
Surya-Abhinai <abhinayj848430@gmail.com>
2024-04-24 23:16:56 +0530

    File text

--- app/main.py
+++ app/main.py
@@ -3,51 +3,65 @@ import threading
 import os
 import sys
 
-
-def handle(conn,dir):
+def handle_client(client_socket, dir):
     while True:
-        data = conn.recv(2048).decode()
-        if data:
-            data = data.split("\r\n")
-            method, path, version = data[0].split(" ")
-
-            if path == '/':
-                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-            elif path.startswith("/echo/"):
-                text = path.split("/echo/")[-1]
-                output = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n"
-                conn.sendall(output.encode())
-            elif path == "/user-agent":
-                text = data[2].split("User-Agent: ")[-1]
-                output = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n"
-                conn.sendall(output.encode())
-
-            elif path.startswith("/files/"):
-                filename = path.split("/files/")[-1]
-                file_path = dir + "/" + filename
-                if os.path.exists(file_path):
-                    with open(file_path, "rb") as f:
-                        data = f.read()
-                        response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(data)}\r\n\r\n"
-                        conn.sendall(response.encode())
-                        conn.sendall(data)
-                else:
-                    conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        data = client_socket.recv(4096).decode()
+        if not data:
+            break
+        data_list = data.split("\r\n")
+        path = data_list[0].split(" ")[1]
+        if str(path) == "/":
+            client_socket.sendall(b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n")
+        elif "/echo/" in str(path):
+            msg = path.split("/echo/")[-1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
+            client_socket.sendall(response.encode())
+        elif str(path) == "/user-agent":
+            user_info = data_list[2].split(" ")[-1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_info)}\r\n\r\n{user_info}"
+            client_socket.sendall(response.encode())
+        elif "/files/" in str(path):
+            filename = path.split("/files/")[-1]
+            file_path = dir + "/" + filename
+            if os.path.exists(file_path):
+                with open(file_path, "rb") as f:
+                    data = f.read()
+                    contentLength = len(data)
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength}\r\n\r\n"
+                    client_socket.sendall(response.encode())
+                    client_socket.sendall(data)
             else:
-                conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
-
-
+                client_socket.sendall(
+                    b"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+                )
+        else:
+            client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            client_socket.sendall(
+                b"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+            )
+    client_socket.close()
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     while True:
-        conn, addr = server_socket.accept()  # wait for client
-        dire = None
+        client_socket, _ = server_socket.accept()
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         if len(sys.argv) > 1:
-            dire = sys.argv[2]
-        client_handler = threading.Thread(target=handle, args=(conn, dire))
+            dir = sys.argv[2]
+            client_handler = threading.Thread(
+                target=handle_client,
+                args=(
+                    client_socket,
+                    dir,
+                ),
+            )
+        else:
+            client_handler = threading.Thread(
+                target=handle_client,
+                args=(
+                    client_socket,
+                    None,
+                ),
+            )
         client_handler.start()
-
-
-
 if __name__ == "__main__":
     main()
