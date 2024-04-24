7a136ef48b820812355833cce70e8d740c76ce97
Surya-Abhinai <abhinayj848430@gmail.com>
2024-04-24 22:31:47 +0530

    Respond with text

--- app/main.py
+++ app/main.py
@@ -4,19 +4,19 @@ import re
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    pattern = r'/echo/(.*)'
-    echo = re.compile(pattern)
+    # pattern = r'/echo/(.*)'
+    # echo = re.compile(pattern)
     while True:
         data = conn.recv(2048).decode()
         if data:
             data = data.split("\r\n")
             method,path,version = data[0].split(" ")
-            match = echo.search(path)
+            # match = echo.search(path)
             if path == '/':
                 conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-            elif match:
-                text = match.group(1)
-                output = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n{text}\r\n"
+            elif path.startswith("/echo/"):
+                text = path.split("/echo/")[-1]
+                output = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n"
                 conn.sendall(output.encode())
             else:
                 conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
