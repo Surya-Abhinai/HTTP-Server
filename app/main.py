babb32a4a6d45d13f3ef8e59e30b4e69137f05a5
Surya-Abhinai <abhinayj848430@gmail.com>
2024-04-24 22:05:34 +0530

    Respond with 404

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn , addr = server_socket.accept() # wait for client
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn, addr = server_socket.accept() # wait for client
+    while True:
+        data = conn.recv(2048).decode()
+        if data:
+            data = data.split("\r\n")
+            method , path , version = data[0].split(" ")
+
+            if path == '/':
+                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            else:
+                conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
 
 
 if __name__ == "__main__":
